c00c5e4ae7f1655488413e49ba567ccbcca91d21
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-10-10 15:20:12 +0330

    add comment

--- app.js
+++ app.js
@@ -4,64 +4,64 @@ import {
   readProducts,
   updateProduct,
   currentProductId,
-} from './api.js';
-import { debounce, gatherFormData, clearInputs } from './utility.js';
+} from "./api.js";
+import { debounce, gatherFormData, clearInputs } from "./utility.js";
 
 export let queryString;
 export let currentSort;
 export let currentPage = 1;
 
-export const productForm = document.querySelector('#create-product');
+export const productForm = document.querySelector("#create-product");
 
 // EVENT LISTENERS
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   readProducts();
 });
 
-document.querySelector('#btn-reset').addEventListener('click', () => {
+document.querySelector("#btn-reset").addEventListener("click", () => {
   clearInputs();
-  productForm.querySelector('#btn-add-product').innerHTML = 'Add Product';
+  productForm.querySelector("#btn-add-product").innerHTML = "Add Product";
 });
 
-document.querySelector('#price').addEventListener('keypress', (event) => {
+document.querySelector("#price").addEventListener("keypress", (event) => {
   if (event.keyCode < 46 || event.keyCode > 57) event.preventDefault();
 });
 
 document
-  .querySelector('#countInStock')
-  .addEventListener('keypress', (event) => {
+  .querySelector("#countInStock")
+  .addEventListener("keypress", (event) => {
     if (event.keyCode < 46 || event.keyCode > 57) event.preventDefault();
   });
 
 const debouncedReadProducts = debounce(readProducts);
-document.querySelector('#searchBox').addEventListener('input', (event) => {
+document.querySelector("#searchBox").addEventListener("input", (event) => {
   queryString = event.target.value.toLowerCase();
   debouncedReadProducts(queryString);
   resetPagination();
 });
 
-document.querySelector('#sortType').addEventListener('change', (event) => {
+document.querySelector("#sortType").addEventListener("change", (event) => {
   currentSort = event.target.value;
   readProducts();
   resetPagination();
 });
 
-document.querySelector('ul.pagination').addEventListener('click', (event) => {
-  const lis = document.querySelectorAll('.page-item');
-  lis.forEach((li) => li.classList.remove('active'));
-  event.target.parentElement.classList.add('active');
+document.querySelector("ul.pagination").addEventListener("click", (event) => {
+  const lis = document.querySelectorAll(".page-item");
+  lis.forEach((li) => li.classList.remove("active"));
+  event.target.parentElement.classList.add("active");
   currentPage = Number(event.target.innerText);
   readProducts();
 });
 
 document
-  .querySelector('#confirm-delete-btn')
-  .addEventListener('click', (event) => {
+  .querySelector("#confirm-delete-btn")
+  .addEventListener("click", (event) => {
     deleteProduct(currentProductId);
   });
 
-productForm.addEventListener('submit', determineCrudMode);
+productForm.addEventListener("submit", determineCrudMode);
 
 // END OF EVENT LISTENERS
 
@@ -82,7 +82,9 @@ function determineCrudMode(event) {
 
 export function resetPagination() {
   currentPage = 1;
-  const lis = document.querySelectorAll('.page-item');
-  lis.forEach((li) => li.classList.remove('active'));
-  lis[0].classList.add('active');
+  const lis = document.querySelectorAll(".page-item");
+  lis.forEach((li) => li.classList.remove("active"));
+  lis[0].classList.add("active");
 }
+
+// This is a test
