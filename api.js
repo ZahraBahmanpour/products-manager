17205bc4a994287628724633db51dc3de3e23ca6
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-11-29 12:05:15 +0330

    replace single with double quot

--- api.js
+++ api.js
@@ -1,26 +1,26 @@
-import { productForm, queryString, currentSort, currentPage } from './app.js';
+import { productForm, queryString, currentSort, currentPage } from "./app.js";
 import {
   clearInputs,
   createPagination,
   gatherFormData,
   generateQueryParams,
   showToast,
-} from './utility.js';
+} from "./utility.js";
 
-const API_URL = 'https://62a4d0d547e6e4006398b48b.mockapi.io/api';
+const API_URL = "https://6300a18859a8760a757d441c.mockapi.io";
 
 export let currentProductId;
 
-const productsTable = document.querySelector('#products tbody');
+const productsTable = document.querySelector("#products tbody");
 
 /////// CREATE ///////
 export async function createNewProduct(newProduct) {
   try {
     const res = await fetch(`${API_URL}/products`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
       body: JSON.stringify(newProduct),
     });
@@ -28,14 +28,14 @@ export async function createNewProduct(newProduct) {
     addToDOM(createdProduct);
     clearInputs();
   } catch (error) {
-    showToast('Problem occured while creating new product');
+    showToast("Problem occured while creating new product");
     console.log(error.message);
   }
 }
 
 // READ
 export async function readProducts() {
-  productsTable.innerHTML = '';
+  productsTable.innerHTML = "";
   try {
     const res = await fetch(
       `${API_URL}/products${generateQueryParams(
@@ -49,7 +49,7 @@ export async function readProducts() {
     createPagination(count);
     products.forEach(addToDOM);
   } catch (error) {
-    showToast('Problem occured while reading products!');
+    showToast("Problem occured while reading products!");
     console.log(error.message);
   }
 }
@@ -58,29 +58,28 @@ async function readProduct(id) {
     const res = await fetch(`${API_URL}/products/${id}`);
     return res.json();
   } catch (error) {
-    showToast('Problem occured while reading product details!');
+    showToast("Problem occured while reading product details!");
     console.log(error.message);
   }
 }
 /////// UPDATE ///////
-export async function updateProduct(event, product) {
-  event.preventDefault();
+export async function updateProduct(product) {
   const updatedProduct = gatherFormData();
   try {
     const updatedItem = await updateOnBackend(updatedProduct, product.id);
     updateOnFrontEnd(updatedItem);
   } catch (error) {
-    showToast('Problem occured while updating product!');
+    showToast("Problem occured while updating product!");
     console.log(error.message);
   }
 }
 
 async function updateOnBackend(updatedProduct, id) {
   const res = await fetch(`${API_URL}/products/${id}`, {
-    method: 'PUT',
+    method: "PUT",
     body: JSON.stringify(updatedProduct),
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
   return await res.json();
@@ -88,7 +87,7 @@ async function updateOnBackend(updatedProduct, id) {
 
 function updateOnFrontEnd(product) {
   const productRow = productsTable.querySelector(`tr[data-id="${product.id}"]`);
-  productRow.innerHTML = '';
+  productRow.innerHTML = "";
   const { nameCell, priceCell, countCell, createDateCell, actionCell } =
     generateTableCells(product);
   productRow.appendChild(nameCell);
@@ -96,31 +95,31 @@ function updateOnFrontEnd(product) {
   productRow.appendChild(countCell);
   productRow.appendChild(createDateCell);
   productRow.appendChild(actionCell);
-  document.querySelector('#btn-add-product').innerHTML = 'Add Product';
+  document.querySelector("#btn-add-product").innerHTML = "Add Product";
   clearInputs();
   currentProductId = undefined;
-  showToast('Successfully Updated', 'green');
+  showToast("Successfully Updated", "green");
 }
 /////// DELETE ///////
 export async function deleteProduct(productId) {
   try {
     const res = await fetch(`${API_URL}/products/${productId}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
     const data = await res.json();
-    showToast('Successfully Deleted');
+    showToast("Successfully Deleted");
     readProducts();
   } catch (error) {
-    showToast('Problem occured deleting the product!');
+    showToast("Problem occured deleting the product!");
     console.log(error.message);
   }
 }
 
 // UPDATE DOM
 function addToDOM(product) {
-  const productRow = document.createElement('tr');
+  const productRow = document.createElement("tr");
   productRow.dataset.id = product.id;
-  productRow.style.lineHeight = '40px';
+  productRow.style.lineHeight = "40px";
   const { nameCell, priceCell, countCell, createDateCell, actionCell } =
     generateTableCells(product);
 
@@ -134,49 +133,49 @@ function addToDOM(product) {
 }
 
 function generateTableCells(product) {
-  const nameCell = document.createElement('td');
+  const nameCell = document.createElement("td");
   nameCell.innerHTML = product.name;
   nameCell.title = product.name;
 
-  const priceCell = document.createElement('td');
+  const priceCell = document.createElement("td");
   priceCell.innerHTML = product.price;
   priceCell.title = product.price;
 
-  const countCell = document.createElement('td');
+  const countCell = document.createElement("td");
   countCell.innerHTML = product.countInStock;
   countCell.title = product.countInStock;
 
-  const createDateCell = document.createElement('td');
+  const createDateCell = document.createElement("td");
   const date = new Date(product.createdAt).toDateString();
   createDateCell.innerHTML = date;
   createDateCell.title = date;
 
-  const viewButton = document.createElement('button');
+  const viewButton = document.createElement("button");
   viewButton.dataset.id = product.id;
   viewButton.innerHTML = '<i class="bi bi-eye"></i>';
-  viewButton.title = 'VIEW';
-  viewButton.className = 'btn btn-warning btn-sm m-1 text-white';
-  viewButton.dataset.bsToggle = 'modal';
-  viewButton.dataset.bsTarget = '#viewModal';
-  viewButton.addEventListener('click', () => viewProduct(product));
+  viewButton.title = "VIEW";
+  viewButton.className = "btn btn-warning btn-sm m-1 text-white";
+  viewButton.dataset.bsToggle = "modal";
+  viewButton.dataset.bsTarget = "#viewModal";
+  viewButton.addEventListener("click", () => viewProduct(product));
 
-  const editButton = document.createElement('button');
+  const editButton = document.createElement("button");
   editButton.dataset.id = product.id;
   editButton.innerHTML = '<i class="bi bi-pen"></i>';
-  editButton.title = 'UPDATE';
-  editButton.className = 'btn btn-primary btn-sm';
-  editButton.addEventListener('click', () => editProduct(product));
+  editButton.title = "UPDATE";
+  editButton.className = "btn btn-primary btn-sm";
+  editButton.addEventListener("click", () => editProduct(product));
 
-  const deleteButton = document.createElement('button');
+  const deleteButton = document.createElement("button");
   deleteButton.dataset.id = product.id;
   deleteButton.innerHTML = '<i class="bi bi-trash"></i>';
-  deleteButton.title = 'DELETE';
-  deleteButton.className = 'btn btn-danger btn-sm m-1';
-  deleteButton.dataset.bsToggle = 'modal';
-  deleteButton.dataset.bsTarget = '#deleteModal';
-  deleteButton.addEventListener('click', () => (currentProductId = product.id));
+  deleteButton.title = "DELETE";
+  deleteButton.className = "btn btn-danger btn-sm m-1";
+  deleteButton.dataset.bsToggle = "modal";
+  deleteButton.dataset.bsTarget = "#deleteModal";
+  deleteButton.addEventListener("click", () => (currentProductId = product.id));
 
-  const actionCell = document.createElement('td');
+  const actionCell = document.createElement("td");
   actionCell.appendChild(viewButton);
   actionCell.appendChild(editButton);
   actionCell.appendChild(deleteButton);
@@ -184,16 +183,16 @@ function generateTableCells(product) {
 }
 
 function editProduct(product) {
-  productForm.querySelector('#name').value = product.name;
-  productForm.querySelector('#price').value = product.price;
-  productForm.querySelector('#countInStock').value = product.countInStock;
-  productForm.querySelector('#btn-add-product').innerHTML = 'Update Product';
+  productForm.querySelector("#name").value = product.name;
+  productForm.querySelector("#price").value = product.price;
+  productForm.querySelector("#countInStock").value = product.countInStock;
+  productForm.querySelector("#btn-add-product").innerHTML = "Update Product";
   currentProductId = product.id;
 }
 
 async function viewProduct(product) {
   const productWithDetails = await readProduct(product.id);
-  const viewModal = document.querySelector('#viewModal .modal-body');
+  const viewModal = document.querySelector("#viewModal .modal-body");
   viewModal.innerHTML = `
   <div class="card p-3 m-3">
   <div class="row"><div class="col-4"><strong>Description</strong></div><div class="col-8">${productWithDetails.description}</div></div>
